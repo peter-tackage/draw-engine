240ebd16862a43664433d2b90b33b356971e6c97
peter-tackage <peter.tackage@gmail.com>
2012-11-12 22:08:56 +0100

    Minor cleanup.

--- src/main/java/com/moac/drawengine/InvalidDrawEngineException.java
+++ src/main/java/com/moac/drawengine/InvalidDrawEngineException.java
@@ -3,8 +3,8 @@ package com.moac.drawengine;
 @SuppressWarnings("serial")
 public class InvalidDrawEngineException extends Exception {
 
-	public InvalidDrawEngineException(String string, Exception e) {
-		super(string, e);
+	public InvalidDrawEngineException(String msg, Exception e) {
+		super(msg, e);
 	}
 
 }
